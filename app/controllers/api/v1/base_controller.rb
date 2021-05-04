bd6a467b5a6c65e4d4de38637654625fabf0ab25
Abrahan Mendoza <mendozaabrahan@gmail.com>
2021-05-04 21:35:50 +0200

    [[FEAT]] Base api controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Base api controller class to handle common logic
+    class BaseController < ApplicationController
+    end
+  end
+end
